59d92c331d3318238b80e9acdfe35038aa15f62a
Bartamel <71917332+Bartamel@users.noreply.github.com>
2021-10-13 17:52:05 -0500

    Create Primerproblema.py

---
+++ Primerproblema.py
@@ -0,0 +1,10 @@
+i = 1
+while i < 101:
+    print (i)
+    if (i % 3 == 0 and i % 5 == 0):
+        print("mareigua")
+    elif (i % 3 == 0):
+        print("mare")
+    elif (i % 5 == 0):
+        print("igua")
+    i += 1
